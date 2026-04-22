ee122dbfa16787b2a5a85ade7316a742d98c480e
emidres135 <113696737+emidres135@users.noreply.github.com>
2026-04-22 14:02:00 +0200

    Delete paginaCBD directory

--- paginaCBD/js/script.js
+++
@@ -1,9 +0,0 @@
-// 1. Accedemos a los nodos del DOM por su ID
-const btnMenu = document.getElementById('btn-menu');
-const menuList = document.getElementById('menu-list');
-
-// 2. Añadimos un escuchador de eventos (listener) al botón
-btnMenu.addEventListener('click', () => {
-    // 3. Alternamos la clase 'mostrar'. Si la tiene, se la quita; si no la tiene, se la pone.
-    menuList.classList.toggle('mostrar');
-});
